bdea6727b49792799a5f0217754d5e09e370bfce
芮智鹏 <ruizhipeng001@gmail.com>
2018-04-23 12:16:57 +0800

    值接收者 指针接收者 值接收者只能处理接收者副本的值 但是不能改变原接收者本身的值 指针接收者会直接对接收者本身进行处理 (直接对原地址的数据进行操作)

---
+++ 027/methods-pointers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {	//指针操作的是接收者的地址，非指针的话就会先赋值一个新地址 然后操作新地址，不会改变接收者原本的值
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
+}
